8096769b7c248029a9d93b29cd97dc1a6eede2d5
Jadid Alam <j.alam@se23.qmul.ac.uk>
2025-03-24 10:00:32 +0000

    1.1
    - changed design to match figma design
    - implemented a searchable select bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import Select from 'react-select';
 import './App.css';
 import img200 from './images/weatherIcons/200.png';
 import img300 from './images/weatherIcons/300.png';
@@ -7,6 +8,10 @@ import img600 from './images/weatherIcons/600.png';
 import img700 from './images/weatherIcons/700.png';
 import img800 from './images/weatherIcons/800.png';
 import img801 from './images/weatherIcons/801.png';
+import dangerLow from './images/dangerLow.png';
+import dangerMid from './images/dangerMid.png';
+import dangerHigh from './images/dangerHigh.png';
+import logo from './images/logo.png';
 import { Line } from "react-chartjs-2"
 import {Chart as ChartJs, Legend, plugins, scales, Tooltip} from "chart.js/auto";
 
@@ -17,9 +22,6 @@ for simplicity we will use one pic for each weather type and not differentiate b
 */
 
 function App() {
-  // getting current date in unix format
-  const currentDateUnix = Math.floor(new Date()/1000);
-  
   // setting up variables for the weather buttons
   let firstDate = new Date();
   firstDate.setDate(new Date().getDate()-3);
@@ -31,86 +33,90 @@ function App() {
     buttonDays.push(dayNames[firstDate.getDay()]);
     firstDate.setDate(firstDate.getDate() + 1);
   }
+  firstDate = new Date();
 
   // initiallizing state variables
-  const [dailyWeather, setDailyWeather] = useState([img200,img300,img500,img600,img700,img800,img801]);
-  const [hourlyTemp, sethourlyTemp] = useState([]);
-  const [locations, setLocations] = useState(["loaction","temp1"]);
-  const [danger, setDanger] = useState(0);
-  const [dangerText, setDangerText] = useState("LOW");
-  useEffect(() => {
-    if (danger === 0) {
-      setDangerText("LOW");
-    }
-    else if (danger === 1) {
-      setDangerText("OK");
-    }
-    else {
-      setDangerText("HIGH");
-    }
-  },[danger]);
-
+  const [dailyWeather,setDailyWeather] = useState([img200,img300,img500,img600,img700,img800,img801]);
+  const locations= useState(["loaction","temp1"]); // change this
+  const locationOptions = locations.map(location => ({
+    value: location,
+    label: location,
+  }));
+  const motorbikeTypeOptions = [
+    { value: "1", label: "Sports" },
+    { value: "2", label: "Scooter" },
+    { value: "3", label: "Cruiser" },
+    { value: "4", label: "Off-road" },
+    { value: "5", label: "Street" },
+  ];
   const [lineChartData, setLineChartData] = useState({
-    labels: ["00","","","03","","","06","","","09","","","12","","","15","","","18","","","21","",""],
+    labels: ["00:00","","","03:00","","","06:00","","","09:00","","","12:00","","","15:00","","","18:00","","","21:00","",""],
     datasets: [{
-      label: "Hourly Temprature",
+      label: "Hourly Temperature",
       data: [25,28,22,30,27,26,24,21,29,23,22,20,26,27,30,28,25,23,22,21,29,30,27,24],
       fill: true,
-      backgroundColor: '#214aa6',
-      borderColor: '#214aa6',
+      backgroundColor: 'rgba(37, 54, 82, 0.8)',
+      borderColor: '#4C71AD',
       borderWidth: 1,
       pointRadius: 0,
       tension: 0.2
-    }] 
-    
+    }]
   });
+  const [selectedDay, setSelectedDay] = useState(3);
 
-  const [data, setData] = useState([]);
-
-  // weather api key
-  const apiKey = "718d0502e1fdf804f510f63140737137";
-
-  // fetching weather data
-  let lat = 51.5;
-  let lon = 0.1;
-  /*
-  let fiveDaysURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`; NEED TO WAIT FOR UNI INSTRUCTIONS COS I NEED THE STUDENT VERSION
-
-  const req = new XMLHttpRequest();
-  req.open("GET", fiveDaysURL);
-  req.onload = () => {
-    setData(req.response);
-  }
-  req.send();
-
-  console.log("PRINTINGNNNNN \n" + data + "\n END");
-
-  // things reqiuired from user
-   - location
-   - date viewing for the hourly (if we have time)
-   - motorbike type for the wind speed etc
-  */
  
   // base styles 
-  const allStyle = `h-[100vh] min-h-[670px] min-w-[365px] grid grid-rows-[10%_80%_10%] xl:grid-rows-[20%_60%_20%] text-xs xl:text-3xl text-blue-700`;
-  const headerStyle = "text-left text-2xl xl:text-5xl font-bold p-2 xl:p-6";
-  const mainStyle = "z-30 grid grid-cols-[5%_90%_5%] xl:grid-cols-[20%_60%_20%]";
-  const insideMainStyle = "col-start-2 grid grid-cols-1 grid-rows-3 xl:grid-cols-3 xl:grid-rows-2 bg-white bg-opacity-85";
-  const weatherBoxStyle = "row-start-1 xl:col-start-1 xl:col-span-2 xl:row-start-1 flex justify-center";
-  const hourlyWeatherStyle = "row-start-2 xl:col-start-1 xl:col-span-2 xl:row-start-2 m-2 xl:m-4";
-  const motorbikeStyle = "row-start-3 xl:col-start-3 xl:row-span-2 xl:row-start-1 border-[1px] rounded-[3px] grid grid-rows-[15%_15%_10%_60%] grid-cols-1 xl:grid-cols-2 xl:grid-rows-[10%_10%_80%] m-1 xl:m-2 text-xs xl:text-2xl p-1";
-  const weatherButtonStyle = "w-[12%] h-[70%] m-1 xl:m-2 border-[1px] border-blue-500 rounded-[3px] grid grid-rows-[20%_60%_20%] bg-gradient-to-t from-[#000340] to-blue-500 text-white text-center text-1XL xl:text-2XL font-bold";
-  const dangerIconStyle = "xl:col-span-2 mx-auto my-auto";
-  const dangerTextStyle = "xl:col-span-2 ml-1 xl:ml-2 mt-auto text-xs xl:text-base text-black font-bold";
-  const footerStyle = "text-center text-xxs xl:text-sm mt-6 xl:mt-20";
-  const gradientStyle = "from-[#000340] via-[#FFFFFF] to-[#FFFFFF]";
-  const inputsStyle = "border-b-[1px] border-blue-500 rounded-[3px] m-1 xl:m-2 text-xs xl:text-base";
+  const allStyle = `h-screen min-h-[670px] min-w-[365px] grid grid-rows-[5%_90%_5%] xl:grid-rows-[20%_60%_20%] text-xs xl:text-3xl ${(firstDate.getHours() >= 6 && firstDate.getHours() <= 18) ? `bg-style-light text-style-dark` : `bg-style-dark text-style-dark`} `;
+  const mainStyle = "z-30 xl:grid grid-cols-[5%_90%_5%] xl:grid-cols-[10%_80%_10%]";
+  const insideMainStyle = "col-start-2 grid grid-cols-1 grid-rows-[2fr_1fr_4fr] xl:grid-cols-3 xl:grid-rows-2 white";
+  const weatherBoxStyle = "row-start-2 xl:col-start-1 xl:col-span-2 flex justify-center border-0 bg-[#253652] rounded-[15px] xl:rounded-[50px] mx-1 xl:mx-0";
+  const hourlyWeatherStyle = "row-start-1 xl:col-start-1 xl:col-span-2 m-2 xl:m-4 grid grid-cols-[1fr_4fr] ";
+  const motorbikeStyle = "row-start-3 xl:col-start-3 xl:row-span-2 xl:row-start-1 bg-[#253652] border-0 rounded-[15px] xl:rounded-[50px] p-4 flex-row m-1 xl:m-2 text-xs xl:text-2xl h-full";
+  const weatherButtonStyle = "w-[13%] xl:w-[12.5%] m-0.5 xl:m-2 border border-[#253652]  rounded-[12px] xl:rounded-[40px] grid grid-rows-[20%_60%_20%] text-center text-1xl xl:text-2xl font-bold hover:border hover:border-[#4C71AD] ";
+  const dangerTextStyle = "m-1 text-base xl:py-2 xl:mt-4 xl:text-xl font-bold border-t border-[#848C9A]";
+  const footerStyle = "text-center text-xxs xl:text-sm mt-2 xl:mt-10 xl:mt-20 text-blue-600";
+  const gradientStyle = "";
+  const inputsStyle = "border-b-[1px] bg-[#4C71AD] border-0 rounded-[25px] m-1 p-1 text-xs xl:text-base";
   const imageStyle = "w-[80%] mx-auto my-auto";
-  const lowDangerStyle = "bg-[radial-gradient(circle,#FFF,#90EE90)] border-green-500 text-green-700";
-  const mediumDangerStyle = "bg-[radial-gradient(circle,#FFF,#FFDBBB)] border-orange-500 text-orange-700";
-  const highDangerStyle = "bg-[radial-gradient(circle,#FFF,#FF8080)] border-red-500 text-red-700";
-  const bigDangerTextStyle = "text-9xl font-bold text-center";
-
+  const InfoText = `text-base xl:text-xl xl:my-2 font-bold`
+  const selectDivStyle = `p-0`;
+  const reactSelectStyle = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "#4C71AD",
+      border: "0px solid transparent",
+      borderRadius: "25px",
+      padding: "2px",
+      margin: "2px",
+      color: "white",
+      fontSize: "1rem",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "rgba(255, 255, 255, 0.7)",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: "#4C71AD",
+      borderRadius: "25px",
+      margin: "0px",
+      border: "0px",
+      fontSize: "1rem",
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? "#36548C" : "#4C71AD",
+      color: "white",
+      borderRadius: "25px",
+      margin: "auto",
+      width: "98%",
+      border: "0px",
+    }),
+  };
   const chartStyle = {
     responsive: true,
     maintainAspectRatio: false,
@@ -121,66 +127,104 @@ function App() {
         }
       },
       y: {
-        suggestedMin: Math.min(...lineChartData.datasets[0].data) - 2,
-        suggestedMax: Math.max(...lineChartData.datasets[0].data) + 2,
+        borderWidth: 0,
+        grid: {
+          display: false,
+        },
+        suggestedMin: Math.min(...lineChartData.datasets[0].data) - 1,
+        suggestedMax: Math.max(...lineChartData.datasets[0].data) + 1,
       },
     },
   }
 
   return (
     <div className={allStyle + " z-10"}>
-      <header className={`z-30 bg-gradient-to-bl  + ${gradientStyle}`}>
-        <h1 className={headerStyle}>W</h1>
+      <header className={`z-30  + ${gradientStyle}`}>
+        <img src={logo} alt={`logo of the website`} className={`h-[90%] p-1 xl:p-10 w-auto`} />
       </header>
 
       <main className={mainStyle}>
         <div className={insideMainStyle}>
-          <div className={weatherBoxStyle}>
-          {
-            buttonDates.map((date,index) => {
-              return (
-                <button key={index} className={weatherButtonStyle}>
-                  <p>{date}</p>
-                  <img className={imageStyle} src={dailyWeather[index]}/>
-                  <p>{buttonDays[index]}</p>
-                </button>
-              )
-            })
-          }
-          </div>
 
           <div className={hourlyWeatherStyle}>
-            <Line data={lineChartData} options={chartStyle}/>
+            <img className={`my-auto`} src={img200} alt={`average weather image`}/>
+            <div className={`p-2 my-auto xl:p-6 xl:h-[80%]`}>
+              <Line className={`w-[100%]`} data={lineChartData} options={chartStyle}/>
+            </div>
           </div>
 
-          <div className={`${motorbikeStyle}+ " " + ${danger === 0 ? lowDangerStyle : danger === 1 ? mediumDangerStyle : highDangerStyle}`}>
-            <select className={inputsStyle} placeholder="Motorbike Type" defaultValue={0}>
-              <option value="0">Motorbike Type</option>
-              <option value="1">Sports</option>
-              <option value="2">Scooter</option>
-              <option value="3">Cruiser</option>
-              <option value="4">Off-road</option>
-              <option value="5">Street</option>
-            </select>
-            <select className={inputsStyle}>
-              {
-                locations.map((location,index) => {
-                  return (
-                    <option key={index} value={location}>{location}</option>
-                  )
-                })
-              }
-            </select>
-            <p className={dangerTextStyle}>Danger Levels:</p>
-            <div className={dangerIconStyle}>
-              <p className={bigDangerTextStyle}>{dangerText}</p>
+          <div className={weatherBoxStyle}>
+            {
+              buttonDates.map((date,index) => {
+                return (
+                    <button key={index} onClick={() => {setSelectedDay(index)}} className={`${weatherButtonStyle} ${firstDate.getDate() === date ? ` text-blue-700` : ``} ${selectedDay === index ? ` bg-[#4C71AD]` : ``}`}>
+                      <p>{date}</p>
+                      <img className={imageStyle} src={dailyWeather[index]} alt={`icon of the weather of this date`}/>
+                      <p>{buttonDays[index]}</p>
+                    </button>
+                )
+              })
+            }
+          </div>
+
+          <div className={motorbikeStyle}>
+
+            <div className={selectDivStyle}>
+              <Select
+                  id={`location1`}
+                  classNames={inputsStyle}
+                  options={motorbikeTypeOptions}
+                  placeholder="Motorbike Type"
+                  styles={reactSelectStyle}
+                  isSearchable
+              />
             </div>
-            
+            <p className={InfoText}>Destination:</p>
+            <div className={`grid grid-cols-2`}>
+              <div className={selectDivStyle}>
+                <Select
+                    id={`location1`}
+                    classNames={inputsStyle}
+                    options={locationOptions}
+                    placeholder="From"
+                    styles={reactSelectStyle}
+                    isSearchable
+                />
+
+              </div>
+
+              <div className={selectDivStyle}>
+                <Select
+                    id={`location2`}
+                    classNames={inputsStyle}
+                    options={locationOptions}
+                    placeholder="To"
+                    styles={reactSelectStyle}
+                    isSearchable
+                />
+              </div>
+            </div>
+
+
+            <p className={dangerTextStyle}>Danger Levels:</p>
+            <img src={dangerLow} alt={`danger level meter image`} className={`mx-auto max-w-[100px] xl:max-w-[300px]`} />
+            <div className={`flex w-full border-t border-[#848C9A]`}>
+              <p className={`self-start mr-auto ${InfoText}`}>Weather Conditions:</p>
+              <p className={`self-end ${InfoText}`}>Cloudy</p>
+            </div>
+            <div className={`flex w-full`}>
+              <p className={`self-start mr-auto ${InfoText}`}>Road Conditions:</p>
+              <p className={`self-end ${InfoText}`}>Dry</p>
+            </div>
+            <div className={`flex w-full`}>
+              <p className={`self-start mr-auto ${InfoText}`}>Suggestion:</p>
+              <p className={`self-end ${InfoText}`}>Helmet and gloves!</p>
+            </div>
           </div>
         </div>
       </main>
 
-      <footer className={`z-30 bg-gradient-to-tr  + ${gradientStyle}`}>
+      <footer className={`z-30  + ${gradientStyle}`}>
         <p className={footerStyle}>all rights reserved</p>
       </footer>
     </div>
